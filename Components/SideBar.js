2612f4d795c6b9ffa24383cf52bdaa816757f981
Nyftheart <nyftheart@MacNyft.local>
2022-05-17 15:07:37 +0200

    second commit

--- Components/SideBar.js
+++ Components/SideBar.js
@@ -18,7 +18,6 @@ function SideBar(){
         <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
             <SidebarRow Icon={UsersIcon} Title="Amis" />
             <SidebarRow Icon={UserGroupIcon} Title="Groupe" />
-            <SidebarRow Icon={ShoppingBagIcon} Title="Market" />
             <SidebarRow Icon={DesktopComputerIcon} Title="Watch" />
             <SidebarRow Icon={CalendarIcon} Title="Event" />
             <SidebarRow Icon={ClockIcon} Title="Rapelle" />
